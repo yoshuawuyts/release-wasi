50380fd62032ebbbbe01c4186ac3c1f69ac43fe4
Yosh <github@yosh.is>
2025-09-16 19:28:14 +0200

    exit if publish fails

--- release-rc.sh
+++ release-rc.sh
@@ -4,17 +4,18 @@
 set -x
 
 # our configuration for this run
+DATE="$(date +'%Y-%m-%d')"
 TEMPDIR="tmp"
-NEXT="0.3.0-rc-$(date +'%Y-%m-%d')"
+NEXT="0.3.0-rc-$DATE"
 
 # Define array of repo names (owner/repo format)
 repos=(
   # "webassembly/wasi-random"
   # "webassembly/wasi-clocks"
-  "webassembly/wasi-filesystem"
+  # "webassembly/wasi-filesystem"
   # "webassembly/wasi-sockets"
-  # "webassembly/wasi-cli"
-  # "webassembly/wasi-http"
+  "webassembly/wasi-cli"
+  "webassembly/wasi-http"
 )
 
 # Create a tempdir to clone into
@@ -33,8 +34,8 @@ for repo in "${repos[@]}"; do
 
   # Wait for the release PR to be filed
   sleep 5 # Sleep to prevent race conditions
-  update_run="$(gh run list --workflow "update-0.3.yml" --json databaseId | jq -r '.[0].databaseId')"
-  gh run watch "$update_run"
+  update_run="$(gh run list --workflow "update-0.3.yml" --created "$DATE" --json databaseId | jq -r '.[0].databaseId')"
+  gh run watch "$update_run" || exit 1
 
   # Wait for the CI on the PR to pass before merging
   pr="$(gh pr list --author "app/github-actions" --json number | jq -r '.[0].number')"
@@ -47,8 +48,8 @@ for repo in "${repos[@]}"; do
 
   # Wait for the release to finish releasing
   sleep 5 # Sleep to prevent race conditions
-  release_run="$(gh run list --workflow "update-0.3.yml" --json databaseId | jq -r '.[0].databaseId')"
-  gh run watch "$release_run"
+  release_run="$(gh run list --workflow "update-0.3.yml" --created "$DATE" --json databaseId | jq -r '.[0].databaseId')"
+  gh run watch "$release_run" || exit 1
 
   cd ..
   printf "/n/n"
